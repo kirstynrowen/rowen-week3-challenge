893adcb7585b90997a25dae6198501b533777f07
Kirstyn Rowen <kirstyn.rowen@gmail.com>
2023-02-04 18:16:51 -0700

    added prompts for password critera and testing if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Arrays of password characters
+var letterUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var letterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numericChar = ["1","2","3","4","5","6","7","8","9"];
+var specialChar = ["!","@","#","$","%","?","&","*","(",")","_"]; 
+
+//Present user with prompts based on password criteria
+//Enter password length between 8-128 characters, present error alert if longer or shorter
+function generatePassword() {
+  var approvedCriteria = '';
+  var finalPassword = '';
+  var confirmLength = prompt("Please choose the length of your password (must be between 8 and 128 characters)");
+
+  if (confirmLength <= 7 || confirmLength >= 129) {
+    alert("Password must be between 8 and 128 characters");
+   return;
+  }
+}
+
+//Gather info on remaining criteria
+var addLetterUpper = confirm("Would you like your password to contain uppercase letters?");
+var addLetterLower = confirm("Would you like your password to contain lowercase letters?");
+var addNumericChar = confirm("Would you like your password to contain numbers?");
+var addSpecialChar = confirm("Would you like your password to contain special characters? Options include: ! @ # $ % ? & * ( ) _");
+
+//validate user choices, loop over array values to create password
+if (addLetterUpper === true) {
+  approvedCriteria += letterUpper;
+} if (addLetterLower === true) {
+  approvedCriteria += letterLower;
+} if (addNumericChar === true) {
+  approvedCriteria += numericChar;
+} if (addspecialChar === true) {
+  approvedCriteria += specialChar;
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +47,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
